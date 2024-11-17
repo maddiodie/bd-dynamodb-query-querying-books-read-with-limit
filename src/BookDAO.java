5effc7363793bc9c563a22448e9ea85d46f84cbb
maddiodie <maddiodie@gmail.com>
2024-11-16 20:15:37 -0700

    implemented the BookDAO class ... easy peasy lemon squeezy ... i was able to avoid the mapper instantiation issue and just returned what was necessary instead of tranforming the data unecessarily

--- src/BookDAO.java
+++ src/BookDAO.java
@@ -1,6 +1,12 @@
+import com.amazonaws.services.dynamodbv2.datamodeling.AttributeTransformer;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.QueryResultPage;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BookDAO {
 
@@ -25,8 +31,24 @@ public class BookDAO {
      * @return list of Books for employee id
      */
     public List<Book> getBooksReadByEmployee(String employeeId, String exclusiveStartAsin, int limit) {
-        //TODO: implement
-        return null;
+        Book book = new Book();
+        book.setId(employeeId);
+
+        Map<String, AttributeValue> exclusiveStartKey = null;
+        if (exclusiveStartAsin != null) {
+            exclusiveStartKey = new HashMap<>();
+            exclusiveStartKey.put("id", new AttributeValue().withS(employeeId));
+            exclusiveStartKey.put("asin", new AttributeValue().withS(exclusiveStartAsin));
+        }
+
+        DynamoDBQueryExpression<Book> queryExpression = new DynamoDBQueryExpression<Book>()
+                .withHashKeyValues(book)
+                .withExclusiveStartKey(exclusiveStartKey)
+                .withLimit(limit);
+
+        QueryResultPage<Book> songQueryResults = mapper.queryPage(Book.class, queryExpression);
+
+        return songQueryResults.getResults();
     }
 
 }
